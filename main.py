091f64fc290ee83b7bf5747ae95ffd60cf0819fe
Wolfgang Richter <wolf@cs.cmu.edu>
2014-09-01 22:06:44 -0400

    siegvswolf: initial app implementation

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+class Hero:
+    def __init__(self, stats):
+        self.update(stats.items())
+
+    def update(self, data):
+        for k,v in data:
+            if k == 'class': v = v.replace('-', ' ').title()
+            setattr(self, k, v)
+
+@app.route('/')
+def hello():
+    r = requests.get('https://us.api.battle.net/d3/profile/theonewolf-1275/?locale=en_US&apikey=hgqmvv3kst5q2ng62s6b2d4w4jqsfxxm')
+    rjson = r.json()
+    
+    wolfmax = max([h for h in rjson['heroes'] if h['seasonal'] and h['hardcore']], key=lambda x: x['level'])
+    wolfhero = Hero(wolfmax)
+
+    r = requests.get('https://us.api.battle.net/d3/profile/thespoon3-1397/?locale=en_US&apikey=hgqmvv3kst5q2ng62s6b2d4w4jqsfxxm')
+    rjson = r.json()
+
+    siegmax = max([h for h in rjson['heroes'] if h['seasonal'] and h['hardcore']], key=lambda x: x['level'])
+    sieghero = Hero(siegmax)
+
+    return render_template('index.jinja2',
+                           wolfhero=wolfhero,
+                           sieghero=sieghero)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
