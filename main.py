6bcfa95d2dda1194bdf0747657b6a4d875fa4138
Wolfgang Richter <wolf@cs.cmu.edu>
2015-02-28 17:36:12 -0500

    main.py: adding failure mode if seasonal's don't exist

--- main.py
+++ main.py
@@ -23,42 +23,60 @@ def default():
     profa = bnetconn.get_profile(USER_A)
     profb = bnetconn.get_profile(USER_B)
 
-    profamax = max([h for h in profa.heroes
+    try:
+        profamax = max([h for h in profa.heroes
                         if h['seasonal'] and h['hardcore']],
                         key=lambda x: x['level'])
-    profahero = bnetconn.get_hero(USER_A, profamax['id'])
+    except ValueError:
+        profamax = None
 
-    profbmax = max([h for h in profb.heroes
-                        if h['seasonal'] and h['hardcore']],
-                        key=lambda x: x['level'])
-    profbhero = bnetconn.get_hero(USER_B, profbmax['id'])
-
-    if profbhero.level >= profahero.level:
-        profbclass = 'text-success'
-        profbprogressclass = 'progress-bar-success'
-        profaclass = 'text-danger'
-        profaprogressclass = 'progress-bar-danger'
+    if profamax is not None:
+        profahero = bnetconn.get_hero(USER_A, profamax['id'])
     else:
-        profbclass = 'text-danger'
-        profbprogressclass = 'progress-bar-danger'
-        profaclass = 'text-success'
-        profaprogressclass = 'progress-bar-success'
+        profahero = None
 
-    profbprogresswidth = float(profbhero.level) / (profbhero.level + profahero.level)
-    profaprogresswidth = float(profahero.level) / (profbhero.level + profahero.level)
+    try:
+        profbmax = max([h for h in profb.heroes
+                        if h['seasonal'] and h['hardcore']],
+                        key=lambda x: x['level'])
+    except ValueError:
+        profbmax = None
 
-    profbprogresswidth = '%0.0f%%' % (profbprogresswidth * 100)
-    profaprogresswidth = '%0.0f%%' % (profaprogresswidth * 100)
+    if profbmax is not None:
+        profbhero = bnetconn.get_hero(USER_B, profbmax['id'])
+    else:
+        profbhero = None
 
-    return render_template('index.jinja2',
+    if profahero is not None and profbhero is not None:
+        if profbhero.level >= profahero.level:
+            profbclass = 'text-success'
+            profbprogressclass = 'progress-bar-success'
+            profaclass = 'text-danger'
+            profaprogressclass = 'progress-bar-danger'
+        else:
+            profbclass = 'text-danger'
+            profbprogressclass = 'progress-bar-danger'
+            profaclass = 'text-success'
+            profaprogressclass = 'progress-bar-success'
+
+        profbprogresswidth = float(profbhero.level) / (profbhero.level + profahero.level)
+        profaprogresswidth = float(profahero.level) / (profbhero.level + profahero.level)
+
+        profbprogresswidth = '%0.0f%%' % (profbprogresswidth * 100)
+        profaprogresswidth = '%0.0f%%' % (profaprogresswidth * 100)
+
+        return render_template('index.jinja2',
+                               profahero=profahero,
+                               profbhero=profbhero,
+                               profaclass=profaclass,
+                               profbclass=profbclass,
+                               profaprogressclass=profaprogressclass,
+                               profbprogressclass=profbprogressclass,
+                               profaprogresswidth=profaprogresswidth,
+                               profbprogresswidth=profbprogresswidth)
+    return render_template('index-failed.jinja2',
                            profahero=profahero,
-                           profbhero=profbhero,
-                           profaclass=profaclass,
-                           profbclass=profbclass,
-                           profaprogressclass=profaprogressclass,
-                           profbprogressclass=profbprogressclass,
-                           profaprogresswidth=profaprogresswidth,
-                           profbprogresswidth=profbprogresswidth)
+                           profbhero=profbhero)
 
 @app.errorhandler(404)
 def page_not_found(e):
